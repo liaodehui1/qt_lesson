5abd70f2dc49a86b929e8884f90304fb31fb6e09
liaodehui <1959956996@qq.com>
2019-09-16 23:45:08 +0800

    组件通信

---
+++ vue/component-communication/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+
+    <child1 
+      :msg="msg" 
+      :msg2="msg2" 
+      :appfn="appfn" 
+      v-bind="test"
+      :count="count"
+      :add1="add1"></child1>
+  </div>
+</template>
+
+<script>
+import child1 from '@/components/common/child1'
+export default {
+  name: 'App',
+  components:{
+    child1
+  },
+  data() {
+    return {
+      msg:'给child1的礼物',
+      msg2:'再次给child1礼物',
+      test:{
+        a:1,
+        b:2
+      },
+      count:1
+    }
+  },
+  methods: {
+    appfn(){
+      console.log('我是App.vue里面的方法')
+    },
+    add1(){
+      // console.log('+')
+      this.count++
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/component-communication/src/components/common/child1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h1>{{msg2}}</h1>
+    <button @click="appfn">按钮</button>
+    {{a}}
+    {{b}}
+    <p>------------------</p>
+    <button @click="add1">+</button>
+    {{count}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:['msg','msg2','appfn','a','b','count','add1'],
+  methods: {
+    add(){
+      this.$emit('add')
+    }
+  },
+}
+</script>
+
+<style lang="" scoped>
+  
+</style>
