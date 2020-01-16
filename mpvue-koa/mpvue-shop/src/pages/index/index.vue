dbefd6492db55720e8f5f19e5f8d8a4f726686c3
liaodehui <1959956996@qq.com>
2020-01-16 23:38:22 +0800

    网易严选

---
+++ mpvue-koa/mpvue-shop/src/pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="index">
+    <!-- 头部搜索 -->
+    <div class="search">
+      <div @click="toMapPage">{{cityName}}</div>
+      <div>
+        <input type="text" placeholder="搜索商品" />
+        <span class="icon"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+var amapFile = require('../../utils/amap-wx.js');
+export default {
+  data () {
+    return {
+      cityName: '南昌'
+    }
+  },
+  methods: {
+    toMapPage () {
+      let _this = this
+      // 通过wx.getSetting 先查询一下用户是否授权 "scope.record"
+      wx.getSetting({
+        success: (res) => {
+          // 如果没有同意授权， 打开设置
+          // console.log(res)
+          if (!res.authSetting['scope.userLocation']) {
+            wx.openSetting({
+              success: res => {
+                // 获取授权位置信息
+                _this.getCityName()
+              }
+            })
+          }else {
+            wx.navigateTo({
+              url: '/pages/mappage/main'
+            });
+          }
+        },
+        fail: (err) => {
+          console.log(err)
+        },
+        complete: () => {}
+      })
+    },
+    getCityName () {
+      let _this = this
+      var myAmapFun = new amapFile.AMapWX({key:'5ab2ff0a483586f74e71fe2e638878e0'});
+      myAmapFun.getPoiAround({
+        success: data => {
+          // 成功回调
+          console.log(data)
+        },
+        fail: info => {
+          console.log(info)
+          _this.cityName = '北京'
+        }
+      })
+    } 
+  } 
+}
+</script>
+
+<style lang="less" scoped>
+@import './style.less';
+</style>
