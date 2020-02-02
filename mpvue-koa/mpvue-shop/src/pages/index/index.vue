61bd56324786d197eaa44bbe9cd0982a58db326d
liaodehui <1959956996@qq.com>
2020-02-02 21:58:17 +0800

    网易严选

--- mpvue-koa/mpvue-shop/src/pages/index/index.vue
+++ mpvue-koa/mpvue-shop/src/pages/index/index.vue
@@ -8,23 +8,66 @@
         <span class="icon"></span>
       </div>
     </div>
+    <div class="swiper">
+      <swiper class="swiper-container"
+        indicator-dots="true"
+        autoplay="true"
+        interval="3000"
+        circular="true"
+        duration="500">
+        <block v-for="(item, index) in banner" :key="index">
+          <swiper-item class="swiper-item">
+            <image class="slide-image" :src="item.image_url"></image>
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
+    <div class="channel">
+      <div v-for="(item, index) in channel" :key="index"
+        @click="categoryList(item.id)">
+        <img :src="item.icon_url" />
+        <p>{{item.name}}</p>
+      </div>
+    </div>
+    <div class="brand">
+      <div class="head">
+        品牌制造商直供
+      </div>
+      <div class="content">
+        <div v-for="(item,index) in brandList" :key="index"
+          @click="brandDetail(item.id)">
+          <div>
+            <p>{{item.name}}</p>
+            <p class="price">{{item.floor_price}}</p>
+          </div>
+          <img :src="item.new_pic_url" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 var amapFile = require('../../utils/amap-wx.js');
 import { mapState, mapMutations } from 'vuex'
+import { get } from '../../utils/index'
 
 export default {
   data () {
     return {
-      
+      banner: [],
+      channel: [],
+      brandList: []
     }
   },
   computed: {
     ...mapState({
       cityName: state => state.cityName
     })
+  },
+  mounted () {
+    this.getCityName()
+    this.getData()
   },
   methods: {
     ...mapMutations(['update']),
@@ -57,10 +100,13 @@ export default {
     getCityName () {
       let _this = this
       var myAmapFun = new amapFile.AMapWX({key:'5ab2ff0a483586f74e71fe2e638878e0'});
-      myAmapFun.getPoiAround({
+      myAmapFun.getRegeo({
         success: data => {
           // 成功回调
-          console.log(data)
+          // console.log(data)
+          _this.update({
+            cityName: data[0].regeocodeData.addressComponent.city
+          })
         },
         fail: info => {
           console.log(info)
@@ -69,7 +115,24 @@ export default {
           })
         }
       })
-    } 
+    },
+    async getData () {
+      const data = await get('/index/index')
+      console.log(data)
+      this.banner = data.banner
+      this.channel = data.channel,
+      this.brandList = data.brandList
+    },
+    categoryList (id) {
+      wx.navgateTo({
+        url: 'pages/categorylist/main?id' + id
+      })
+    },
+    brandDetail (id) {
+      wx.navgateTo({
+        url: 'pages/branddetail/main?id' + id
+      })
+    }
   } 
 }
 </script>
