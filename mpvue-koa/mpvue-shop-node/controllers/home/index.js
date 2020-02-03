9ad7536bb07f85560466dcf1f25207b232c9c732
liaodehui <1959956996@qq.com>
2020-02-03 22:27:42 +0800

    网易严选后台

--- mpvue-koa/mpvue-shop-node/controllers/home/index.js
+++ mpvue-koa/mpvue-shop-node/controllers/home/index.js
@@ -10,11 +10,24 @@ module.exports = async (ctx) => {
   const channel = await mysql('nideshop_channel').select() 
   
   // 品牌列表
-  const brandList = await mysql('nideshop_brand').select()
+  const brandList = await mysql('nideshop_brand').where({
+    is_new: 1
+  }).orderBy('sort_order').select()
 
+  // 新品首发
+  const newGoods = await mysql('nideshop_goods')
+    .whereIn('id', [1181000, 1135002, 1134030, 1134032])
+    .andWhere('is_new', 1).select()
+  
+  // 人气推荐
+  const hotGoods = await mysql('nideshop_goods')
+    .column('id', 'name', 'list_pic_url', 'retail_price', 'goods_brief')
+    .where({is_hot: 1}).limit(5).select()
   ctx.body = {
     'banner': banner,
     'channel': channel,
-    'brandList': brandList
+    'brandList': brandList,
+    'newGoods': newGoods,
+    'hotGoods': hotGoods
   }
 }
