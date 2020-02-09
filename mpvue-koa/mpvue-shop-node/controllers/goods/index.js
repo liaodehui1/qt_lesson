a29ba332f35736f2cb88a44b5cb54e5cfe3ec2f3
liaodehui <1959956996@qq.com>
2020-02-10 00:40:31 +0800

    网易严选后台

--- mpvue-koa/mpvue-shop-node/controllers/goods/index.js
+++ mpvue-koa/mpvue-shop-node/controllers/goods/index.js
@@ -4,17 +4,29 @@ const { mysql } = require('../../mysql')
 async function detailAction (ctx) {
   const goodsId = ctx.query.id
   const openId = ctx.query.openId
+
   // 商品信息
   const info = await mysql('nideshop_goods').where({
     'id': goodsId
   }).select()
+
   // 获取商品图片
   const gallery = await mysql('nideshop_goods_gallery').where({
     'goods_id': goodsId
   })
+
+  // 商品参数
+  // 关联查询 nideshop_goods_attribute和nideshop_attribute两张表
+  const attribute = await mysql('nideshop_goods_attribute')
+    .leftJoin('nideshop_attribute', 'nideshop_goods_attribute.attribute_id', 'nideshop_attribute.id')
+    .where({ 'nideshop_goods_attribute.goods_id': goodsId })
+    .column('nideshop_goods_attribute.value', 'nideshop_attribute.name')
+    .select()
+
   ctx.body = {
-    info,
-    gallery
+    info: info[0] || {},
+    gallery,
+    attribute
   }
 }
 
